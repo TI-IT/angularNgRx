caa9ea9be11180505571f2e1ba03ffd9797c7614
TI-IT <go.tiit.ogle@gmail.com>
2023-04-13 10:38:28 +0300

    lesson 4

--- src/app/counter/counter-button/counter-button.component.ts
+++ src/app/counter/counter-button/counter-button.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-counter-button',
   templateUrl: './counter-button.component.html',
   styleUrls: ['./counter-button.component.scss']
 })
-export class CounterButtonComponent {
+export class CounterButtonComponent implements OnInit{
+
+  @Output() increment = new EventEmitter<void>();
+  @Output() decrement = new EventEmitter<void>();
+  @Output() reset = new EventEmitter<void>();
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+
+  onIncrement(){
+    this.increment.emit();
+  }
+
+  onDecrement(){
+    this.decrement.emit();
+  }
+
+  onReset(){
+    this.reset.emit();
+  }
 
 }
