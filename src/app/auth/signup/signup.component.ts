b59b4386d285417f129593a45f0afc82fff6ddb4
TI-IT <go.tiit.ogle@gmail.com>
2023-04-17 17:34:15 +0300

    lesson 31

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Store} from "@ngrx/store";
+import {AppState} from "../../store/app.state";
+import {signupStart} from "../state/auth.actions";
+import {setLoadingSpiner} from "../../store/Shared/shared.actions";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+  signUpForm: FormGroup;
+
+  constructor(private store: Store<AppState>) {
+  }
+  ngOnInit(): void {
+    this.signUpForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    })
+  }
+
+  onSignUpSubmit(){
+    if(!this.signUpForm.valid){
+      return;
+    }
+    const email = this.signUpForm.value.email;
+    const password = this.signUpForm.value.password;
+    this.store.dispatch(setLoadingSpiner({status: true}));
+    this.store.dispatch(signupStart({email, password}));
+  }
 
 }
